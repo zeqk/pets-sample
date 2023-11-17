29adef6f945b5763690d43426e524933e4e6d766
perez.e <perez.e@softlab.umet.edu.ar>
2023-11-17 07:56:49 -0300

    feat: petstore openapi

---
+++ Services/PetStore/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using PetStore;
+
+namespace PetsSample.Services.PetStore
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddPetStoreApiClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions<Options.PetStore>()
+                .Bind(configuration.GetSection(nameof(Options.PetStore)))
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            var config = configuration.GetSection(nameof(Options.PetStore)).Get<Options.PetStore>();
+
+            services.AddHttpClient<PetStoreApiClient>(client =>
+            {
+                client.BaseAddress = new Uri(config.BaseAddress);
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Services/PetStore/Options/PetStore.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetsSample.Services.PetStore.Options;
+
+public class PetStore
+{
+    [Required]
+    public string BaseAddress { get; set; }
+}
